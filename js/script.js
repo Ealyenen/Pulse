385b6b1278e4b08fc94d7670cf01ef2a07a458aa
Ealyenen <Ealyenen@yandex.ru>
2021-09-14 17:43:15 +0300

    slider

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider-section__inner').slick({
+        speed: 1500,
+        focusOnSelect: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/prev_arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/next_arrow.png"></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                arrows: false
+              }
+            },
+          ]
+    });
+  });
