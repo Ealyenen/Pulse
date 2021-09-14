27ad618ce88a874be0b1aff5983244842a99bc4a
Ealyenen <Ealyenen@yandex.ru>
2021-09-14 17:52:24 +0300

    slider buttons fixed

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         focusOnSelect: false,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/prev_arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/next_arrow.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/prev_arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/next_arrow.png"></button>',
         responsive: [
             {
               breakpoint: 1024,
