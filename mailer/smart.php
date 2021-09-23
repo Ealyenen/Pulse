71ce4f50a753b77b54c37c67853ab50f2154e45a
Ealyenen <Ealyenen@yandex.ru>
2021-09-23 23:50:56 +0300

    php2

--- mailer/smart.php
+++ mailer/smart.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('EalyenenPost@yandex.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress($email);     // Add a recipient
+$mail->addAddress(email);     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
